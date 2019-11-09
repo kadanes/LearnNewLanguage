00704db000bf3d23f9cdb6ba8bf1399af5145671
Jayson <jayson.jury@gmail.com>
2019-11-09 14:59:59 +1300

    added translation service to results view. not coming through to results label

--- LearnNewLanguage/ResultsViewController.swift
+++ LearnNewLanguage/ResultsViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     var imageResult: String?
+    var translatedText = ""
+    let jsonEncoder = JSONEncoder()
+    let translateURL = "https://unitec-assignment3-translation.cognitiveservices.azure.com/sts/v1.0/issuetoken"
+    let translateKey = "7c0cc4fccb60499e8c4e767bdcf94539"
 
   
     @IBOutlet weak var resultLabel: UILabel!
@@ -18,13 +22,100 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.resultLabel.text = imageResult
+        translate(toTranslate: imageResult ?? "error occured")
+        self.resultLabel.text =  (imageResult ?? " ") + translatedText
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func returnPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    func translate(toTranslate: String){
+        let fromLangCode = "en"
+        let toLangCode = "de"
+            
+           // print("this is the selected language code ->", arrayLangInfo[fromLangCode].code)
+            
+           // let selectedFromLangCode = arrayLangInfo[fromLangCode].code
+            //let selectedToLangCode = arrayLangInfo[toLangCode].code
+            
+            struct encodeText: Codable {
+                var text = String()
+            }
+            
+            let azureKey = "7c0cc4fccb60499e8c4e767bdcf94539"
+            
+            let contentType = "application/json"
+            let traceID = "A14C9DB9-0DED-48D7-8BBE-C517A1A8DBB0"
+            let host = "dev.microsofttranslator.com"
+            let apiURL = "https://dev.microsofttranslator.com/translate?api-version=3.0&from=" + fromLangCode + "&to=" + toLangCode
+            
+            let text2Translate = toTranslate
+            var encodeTextSingle = encodeText()
+            var toTranslate = [encodeText]()
+            
+        encodeTextSingle.text = text2Translate
+            toTranslate.append(encodeTextSingle)
+            
+            let jsonToTranslate = try? jsonEncoder.encode(toTranslate)
+            let url = URL(string: apiURL)
+            var request = URLRequest(url: url!)
+
+            request.httpMethod = "POST"
+            request.addValue(azureKey, forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
+            request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+            request.addValue(traceID, forHTTPHeaderField: "X-ClientTraceID")
+            request.addValue(host, forHTTPHeaderField: "Host")
+            request.addValue(String(describing: jsonToTranslate?.count), forHTTPHeaderField: "Content-Length")
+            request.httpBody = jsonToTranslate
+            
+            let config = URLSessionConfiguration.default
+            let session =  URLSession(configuration: config)
+            
+            let task = session.dataTask(with: request) { (responseData, response, responseError) in
+                
+                if responseError != nil {
+                    print("this is the error ", responseError!)
+                    
+                    let alert = UIAlertController(title: "Could not connect to service", message: "Please check your network connection and try again", preferredStyle: .actionSheet)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    
+                    self.present(alert, animated: true)
+                    
+                }
+                print("*****")
+                self.parseJson(jsonData: responseData!)
+            }
+            task.resume()
+        }
+        
+        
+        func parseJson(jsonData: Data) {
+            
+            //*****TRANSLATION RETURNED DATA*****
+            struct ReturnedJson: Codable {
+                var translations: [TranslatedStrings]
+            }
+            struct TranslatedStrings: Codable {
+                var text: String
+                var to: String
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            let langTranslations = try? jsonDecoder.decode(Array<ReturnedJson>.self, from: jsonData)
+            let numberOfTranslations = langTranslations!.count - 1
+            print(langTranslations!.count)
+            
+            //Put response on main thread to update UI
+            DispatchQueue.main.async {
+                print(self.translatedText)
+                self.translatedText = langTranslations![0].translations[numberOfTranslations].text
+                print(self.translatedText)
+            }
+        }
+        
     }
     
     /*
@@ -37,4 +128,4 @@ class ResultsViewController: UIViewController {
     }
     */
 
-}
+
